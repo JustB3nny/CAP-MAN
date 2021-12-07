285ce2d8bdb770c42cff0257d9e17537bf48e79c
JustB3nny <60150127+JustB3nny@users.noreply.github.com>
2021-12-07 01:11:17 +0000

    boop <3

--- formGame.cs
+++ formGame.cs
@@ -49,18 +49,34 @@ namespace CAP_MAN
         //Form load
         private void formStartGame_Load(object sender, EventArgs e)
         {
+            //Shows player 2 ui elements if the multiplayer flag is true.
             if (multiplayer)
             {
+                //Changes the top banner to state that the gamemode is multiplayer.
                 lbBanner.Text = "Local Multiplayer";
+                //Displays the "player 2:" lable.
                 lbPlayer2.Visible = true;
+                //Displays the textbox for player 2 to enter their alias.
                 tbPlayer2.Visible = true;
             }
-            pbGame.Hide(); lblP1Score.Hide(); lblP2Score.Hide();
-            cellSize = new SizeF((float)pbGame.Width / gridWidth, (float)pbGame.Height / gridHeight); //Works out the size of one cell. 
-            pbGame.BackgroundImage = new Bitmap(pbGame.Width, pbGame.Height); //Creates a bitmap the size of the panel and sets it as the background.
-            g = Graphics.FromImage(pbGame.BackgroundImage); //Sets g to new graphics on the background image bitmap.
-            loadMaze(wall, ref walls); //Passes the wall array and the List of points (walls) to the proceedure.
-            loadMaze(point, ref points); //Passes the point array and the List of points (points) to the proceedure.
+            //Hides certain elements from showing till the game has started.
+            //Hides the picturebox which the game is rendered to.
+            pbGame.Hide(); 
+            //Hides the lable that displays player 1's score.
+            lblP1Score.Hide(); 
+            //Hides the lable that displays player 2's score.
+            lblP2Score.Hide();
+            //Calculates the size of one grid cell by dividing the width and height of the picturebox by the gridWidth and gridHeight variables.
+            cellSize = new SizeF((float)pbGame.Width / gridWidth, (float)pbGame.Height / gridHeight);
+            //Creates a new bitmap on the background of the picturebox that is the same size and width of it.
+            pbGame.BackgroundImage = new Bitmap(pbGame.Width, pbGame.Height);
+            //Sets g to draw new graphics to the background image of the picturebox. 
+            g = Graphics.FromImage(pbGame.BackgroundImage);
+            //Calls the loadMaze proceedure to populate the list of points for the walls and coins by passing the list and data(by reference).
+            //Walls
+            loadMaze(wall, ref walls);
+            //Coins
+            loadMaze(point, ref points);
         }
         //Start game button
         private void btnStart_Click(object sender, EventArgs e)
@@ -86,6 +102,7 @@ namespace CAP_MAN
                 if (tbPlayer1.TextLength == 3)
                 {
                     p1Name = tbPlayer1.Text.ToUpper();
+                    lbPlayer1.Hide(); lbPlayer1.Hide(); tbPlayer1.Hide(); tbPlayer2.Hide(); btnStart.Hide();
                     pbGame.Show(); lblP1Score.Show(); //Shows the picturebox which the game will be displayed in.
                     draw(); //Calls the draw proceedure.
                     timerTick.Start(); //Starts the timer.
@@ -95,25 +112,6 @@ namespace CAP_MAN
                     MessageBox.Show("You need to have 3 characters for your alias.");
                 }
             }
-            if (tbPlayer1.TextLength == 3)
-            {
-                p1Name = tbPlayer1.Text.ToUpper();
-                if (multiplayer)
-                {
-                    if (tbPlayer2.TextLength == 3)
-                    {
-                        p2Name = tbPlayer2.Text.ToUpper();
-                    }
-                    else
-                    {
-                        MessageBox.Show("You need to have 3 characters for your alias.");
-                    }
-                }
-            }
-        }
-        private void start()
-        {
-            lbPlayer1.Hide(); lbPlayer1.Hide(); tbPlayer1.Hide(); tbPlayer2.Hide(); btnStart.Hide();
         }
 
         //Key press movement.
@@ -176,22 +174,31 @@ namespace CAP_MAN
         private void timerTick_Tick(object sender, EventArgs e)
         {
             //Player movement update.
-            p1Position = new Point(p1Position.X + p1XMove, p1Position.Y + p1YMove); //Update player 1 X & Y position based on what key has been pressed.
-            if (multiplayer) //Only if multiplayer
+            //Updates player 1's X & Y position with the direction the user wishes to move in.
+            p1Position = new Point(p1Position.X + p1XMove, p1Position.Y + p1YMove);
+            //Only if the multiplayer flag is true.
+            if (multiplayer)
             {
-                p2Position = new Point(p2Position.X + p2XMove, p2Position.Y + p2YMove); //Update player 2 X & Y position based on what key has been pressed.
+                //Updates player 2's X & Y position with the direction the user wishes to move in.
+                p2Position = new Point(p2Position.X + p2XMove, p2Position.Y + p2YMove);
             }
             //Wall collision.
+            //Loops through every index in the list of points called walls.
             foreach (Point i in walls)
             {
+                //Compares the point at each index to the player's position.
                 if (i == p1Position)
                 {
+                    //If the player's position is the same as a wall position, it sets the players position back one movement value.
                     p1Position = new Point(p1Position.X - p1XMove, p1Position.Y - p1YMove);
                 }
+                //Only if the multiplayer flag is true.
                 if (multiplayer)
                 {
+                    //Compares the point at each index to the player's position.
                     if (i == p2Position)
                     {
+                        //If the player's position is the same as a wall position, it sets the players position back one movement value.
                         p2Position = new Point(p2Position.X - p2XMove, p2Position.Y - p2YMove);
                     }
                 }
@@ -244,21 +251,22 @@ namespace CAP_MAN
                 g.FillRectangle(Brushes.Blue, walls[i].X * cellSize.Width, walls[i].Y * cellSize.Height, cellSize.Width, cellSize.Height);
             }
             //Draws the maze coins by looping through every index.
-            for (int i = 0; i < points.Count; i++)
+            /*for (int i = 0; i < points.Count; i++)
             {
                 //Draws a coin at specific x & y values obtained by the index of the wall and cell size.
                 g.FillEllipse(Brushes.Orange, points[i].X * cellSize.Width + cellSize.Width / 2, points[i].Y * cellSize.Height + cellSize.Height / 2, 5, 5);
-            }
+            }*/
 
             //g.DrawImage(Image, p1.X * cellSize.Width + cellSize.Width / 4, p1.Y * cellSize.Height, 20, 20);
 
-            //Player rendering.
             //Player 1 rendering.
-            g.FillEllipse(Brushes.Green, p1Position.X * cellSize.Width + cellSize.Width / 4, p1Position.Y * cellSize.Height, 20, 20); //Draws a green circle at the grid coordinates (x,y).
+            //Draws a green circle using the player position points multiplied by the width and height of one cell to create a bitmap coordinate. 
+            g.FillEllipse(Brushes.Green, p1Position.X * cellSize.Width + cellSize.Width / 4, p1Position.Y * cellSize.Height, 20, 20);
             //Player 2 rendering.
+            //Draws a red circle using the player position points multiplied by the width and height of one cell to create a bitmap coordinate. 
             if (multiplayer)
             {
-               g.FillEllipse(Brushes.Red, p2Position.X * cellSize.Width + cellSize.Width / 4, p2Position.Y * cellSize.Height, 20, 20); //Draws a red circle at the grid coordinates (x,y).
+               g.FillEllipse(Brushes.Red, p2Position.X * cellSize.Width + cellSize.Width / 4, p2Position.Y * cellSize.Height, 20, 20);
             }
 
             //Refreshing the picturebox background with the updated graphics/info.
